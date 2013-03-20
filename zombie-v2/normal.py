fab9e1f7a7d233bb5c5098c4ed7451d6e0890ca2
Michelle Naylor <michnaylor@gmail.com>
2013-03-20 13:33:21 -0600

    normals move to homebase

--- zombie-v2/normal.py
+++ zombie-v2/normal.py
@@ -9,6 +9,60 @@ import re
 import zombie
 import defender
 
+# used to calculate distance to homebase
+near_epsilon = 200
+# inititalize global homebase to none
+homebase = None
+
+
+def distance_to_point(person, point):
+    """
+    returns the distance between a person, and a (x, y) point in the playground
+    """
+    delta_x = point[0] - person.get_xpos()
+    delta_y = point[1] - person.get_ypos()
+    d = (delta_x * delta_x + delta_y * delta_y) ** 0.5
+
+    if agentsim.debug.get(32):
+        print("distance between person and point is {}".format(d))
+
+    return d
+
+def get_count_near_point(point, group):
+    """
+    returns the number of person-class members near a (x, y) point
+    """
+    # intialize count to 0
+    count = 0
+    for person in group:
+        if distance_to_point(person, point) < near_epsilon:
+            count = count + 1
+
+    if agentsim.debug.get(32):
+        print("there are {} persons near point {}".format(count, point))
+
+    return count
+
+def set_homebase():
+    """
+    returns the corner with the lowest number of zombies
+    """
+    # find all zombies
+    all_z = zombie.Zombie.get_all_present_instances()
+    # get corner coordinates
+    (x_min, y_min, x_max, y_max) = agentsim.gui.get_canvas_coords()
+    corner_set = ((0,0), (x_max, y_min), (x_min, y_max), (x_max, y_max))
+
+    homebase = min(
+        [(corner, get_count_near_point(corner, all_z)) for corner in corner_set],
+        key = (lambda x:x[1])
+        )
+
+    if agentsim.debug.get(32):
+        print("homebase is {} with {} zombies".format(homebase[0], homebase[1]))
+
+    return homebase[0]
+
 class Normal(MoveEnhanced):
 
     def __init__(self, **keywords):
@@ -21,6 +75,8 @@ class Normal(MoveEnhanced):
 
         self._zombie_alert_args = None
 
+        self._at_home = False
+
         if agentsim.debug.get(2):
             print("Normal", self._name)
 
@@ -28,16 +84,35 @@ class Normal(MoveEnhanced):
         self.set_size(random.uniform(self.get_min_size(), self.get_max_size()))
 
     def get_author(self):
-        return "Your names go here"
-
+        return "Alexander Wong, Michelle Naylor"
+        
     def compute_next_move(self):
-        # if we have a pending zombie alert, act on that first
-        if self._zombie_alert_args is not None:
-            (x, y) = self._zombie_alert_args
-            delta_x = x - self.get_xpos()
-            delta_y = y - self.get_ypos()
-            # clear the alert
-            self._zombie_alert_args = None 
+        # if no homebase, set homebase
+        global homebase
+        if homebase == None:
+            homebase = set_homebase()
+             
+        # move towards homebase if not yet near homebase
+        if self._at_home == False:
+            # if we have a pending zombie alert, act on that first
+            if self._zombie_alert_args is not None:
+                (x, y) = self._zombie_alert_args
+                delta_x = x - self.get_xpos()
+                delta_y = y - self.get_ypos()
+                # clear the alert
+                self._zombie_alert_args = None 
+            # move towards zombie base
+            else:
+                delta_x = homebase[0] - self.get_xpos()
+                delta_y = homebase[1] - self.get_ypos()
+                # if near homebase, set self._at_home
+                if distance_to_point(self, homebase) < near_epsilon:
+                    self._at_home = True
+
+                    if agentsim.debug.get(32):
+                        print("normal {} is at home".format( self.get_name()))
+
+   
         else:
             delta_x = 10 * (0.5 - random.random())
             delta_y = 10 * (0.5 - random.random())
