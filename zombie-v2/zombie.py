de72fb77fb79fb39595566a5a29e344c6839e1f9
awwong1 <alexanderwwong@gmail.com>
2013-03-23 23:42:35 -0600

    Made zombies smarter, focus on unique normals

--- zombie-v2/zombie.py
+++ zombie-v2/zombie.py
@@ -8,49 +8,80 @@ import normal
 import defender
 
 class Zombie(MoveEnhanced):
+    my_target = None
 
     def __init__(self, **keywords):
         MoveEnhanced.__init__(self, **keywords)
         self.set_happiness(1)
-        
         self._default_size = self.get_size()
-
         if agentsim.debug.get(2):
             print("Zombie", self._name)
 
     def get_author(self):
         return "Alexander Wong, Michelle Naylor"
 
-
     def compute_next_move(self):
+
+        """
+        These zombies move to their unique closest normal
+        """
+        
         if agentsim.debug.get(128):
             pass
         delta_x = 0
         delta_y = 0
 
-        # make sure you are your normal size
-        self.set_size(self._default_size)
+        # Alex: Changed to min to increase speed and maneuverability
+        self.set_size(self.get_min_size())
 
-        # find nearest normal if there is one!
         all_n = normal.Normal.get_all_present_instances()
         if all_n:
-            nearest = min(
-                # make pairs of (person, distance from self to person)
-                [ (n, self.distances_to(n)[0] ) for n in all_n ]
-                ,
-                # and sort by distance
-                key=(lambda x: x[1])
-                )
+            audition = []
+            for i in all_n:
+                n_distance = ((i.get_xpos() - self.get_xpos()) ** 2 + (i.get_ypos() - self.get_ypos()) ** 2) ** 0.5
+                audition.append((i, n_distance))
+            audition.sort(key=lambda targets: targets[1])
+                        
+            # Take the nearest normal that isn't already taken
+            all_z = Zombie.get_all_present_instances()
+            for i in all_z:
+                if self == i:
+                    continue
+                for a in audition:
+                    if i.my_target == a[0]:
+                        audition.remove(a)
+                        
+            # Take the nearest normal that isn't already claimed
+            if len(audition) > 0:
+                target = audition[0][0]
+                self.my_target = target
 
-            (near_n, near_d) = nearest
+            # If all the normals are taken, take the nearest one
+            else:
+                self.my_target = None
+                nearest = min(
+                    # make pairs of (person, distance from self to person)
+                    [ (n, self.distances_to(n)[0] ) for n in all_n ]
+                    ,
+                    # and sort by distance
+                    key=(lambda x: x[1])
+                    )
+                (target, target_d) = nearest
 
-            # move towards nearest normal
-            (d, delta_x, delta_y, d_edge_edge) = self.distances_to(near_n)
+            if agentsim.debug.get(128):
+                try:
+                    print("Zombie: ", self.get_name(), " Target: ", self.my_target.get_name())
+                except:
+                    print("Zombie No Target")
+                    pass
+                for i in all_n:
+                    print("Normal: ", i.get_name(), " Distance to me: ", self.distances_to(i)[0])
+                
+            # move towards target
+            (d, delta_x, delta_y, d_edge_edge) = self.distances_to(target)
 
             # if close to target, get bigger
-            if d < 65:
-                old_size = self.get_size()
-                max_size = self.get_max_size()
-                self.set_size(max_size)
+            if d < 25:
+                self.set_size(self.get_max_size())
 
         return (delta_x, delta_y)
